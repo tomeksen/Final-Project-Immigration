9ed5ab3da768a560e5ac171ae3e194707bee9250
Tomas <44404123+tomeksen@users.noreply.github.com>
2024-10-28 00:33:42 -0700

    update version to next 14

--- front-end/immigration-app/next.config.mjs
+++ front-end/immigration-app/next.config.mjs
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
 };
 
--- front-end/immigration-app/src/app/playground/page.tsx
+++ front-end/immigration-app/src/app/playground/page.tsx
@@ -2,7 +2,6 @@ import {useTranslations} from 'next-intl';
 
 const TestPage = () => {
     const translations = useTranslations('HomePage');
-
     return ( 
     <div>
         <h1>{translations('title')}</h1>
