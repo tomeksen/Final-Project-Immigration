cde38495ed079cdf4d1817c7a4fc68605186265a
Joao Sinhasique <sinhasique12@gmail.com>
2024-10-30 11:29:25 -0700

    header + section 1 of the main page

--- front-end/immigration-app/src/components/ThemeLogo.tsx
+++ front-end/immigration-app/src/components/ThemeLogo.tsx
@@ -5,17 +5,19 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 
 import LogoDark from "@/assets/logo/LOGO_WHITE.png";
-import LogoLight from "@/assets/logo/Up_Immigration_Logo.png";
+import LogoLight from "@/assets/logo/logo_up_immigration.png";
 
 export function ThemeLogo() {
   const { theme } = useTheme();
 
   return (
-    <Image
-      src={theme === "dark" ? LogoDark : LogoLight}
-      alt="logo"
-      width={170}
-      height={70}
-    />
+    <>
+      <Image
+        src={theme === "dark" ? LogoDark : LogoLight}
+        alt="logo"
+        className="object-cover w-28" // Maintains aspect ratio without distortion
+        priority
+      />
+    </>
   );
 }
