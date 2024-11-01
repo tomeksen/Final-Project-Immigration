1f550b7b673c77753f3f7b3deeeae243db8297b4
Joao Sinhasique <sinhasique12@gmail.com>
2024-10-31 23:16:53 -0700

    testimonials

--- front-end/immigration-app/src/app/home/page.tsx
+++ front-end/immigration-app/src/app/home/page.tsx
@@ -1,6 +1,7 @@
 import SectionFive from "@/components/mainPage/SectionFive";
 import SectionFour from "@/components/mainPage/SectionFour";
 import SectionOne from "@/components/mainPage/SectionOne";
+import SectionSeven from "@/components/mainPage/SectionSeven";
 import SectionSix from "@/components/mainPage/SectionSix";
 import SectionThree from "@/components/mainPage/SectionThree";
 import SectionTwo from "@/components/mainPage/SectionTwo";
@@ -14,6 +15,7 @@ export default function HomePage() {
       <SectionFour />
       <SectionFive />
       <SectionSix />
+      <SectionSeven />
     </div>
   );
 }
