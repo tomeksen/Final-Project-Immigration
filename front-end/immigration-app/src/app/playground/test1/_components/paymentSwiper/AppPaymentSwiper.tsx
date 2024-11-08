6a31117d65c3e15077db37134e84c31f421c5dcd
yusuken1121 <yusuken1121@gmail.com>
2024-11-08 12:17:07 -0800

    Enhance functionality across multiple slides

--- front-end/immigration-app/src/app/playground/test1/_components/paymentSwiper/AppPaymentSwiper.tsx
+++ front-end/immigration-app/src/app/playground/test1/_components/paymentSwiper/AppPaymentSwiper.tsx
@@ -12,7 +12,11 @@ import { Badge } from "@/components/ui/badge";
 import PaymentSwiperButton from "./paymentSwiperButton";
 import { cn } from "@/lib/utils";
 
-export default function AppPaymentSwiper() {
+type PaymentSwiperProps = {
+  swiperType: "sm" | "lg";
+};
+
+export default function AppPaymentSwiper({ swiperType }: PaymentSwiperProps) {
   // Sample payment data
   type Payment = {
     id: string;
@@ -37,6 +41,8 @@ export default function AppPaymentSwiper() {
     },
   ];
 
+  const slidesPerView = swiperType === "sm" ? 1 : 2;
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const handleSlideChange = (swiper: SwiperType) => {
     const currentIndex = swiper.activeIndex;
@@ -49,62 +55,50 @@ export default function AppPaymentSwiper() {
       <p className="mb-4">
         You have <span className="text-red-500">3 pending payments</span>.
       </p>
-      <div className="flex items-center justify-center">
-        <Swiper
-          spaceBetween={30}
-          onSlideChange={handleSlideChange}
-          className="flex items-center justify-center"
-        >
-          {payments.map((payment, index) => {
-            return (
-              <SwiperSlide key={index} className="z-0">
-                <Card
-                  className="relative overflow-hidden w-[220px] min-h-[181px] mx-auto"
-                  key={index}
-                >
-                  <div className="p-4">
-                    <Badge
-                      variant="outline"
-                      className="mb-2 w-full rounded-full"
-                    >
-                      {payment.id}
-                    </Badge>
-                    <div className="text-gray-700 font-thin text-sm mb-2">
-                      {payment.description}
-                    </div>
-                    <div className="text-2xl">
-                      CAD {payment.amount.toFixed(2)}
-                    </div>
-                  </div>
 
-                  <Button className="absolute bottom-0 right-0 z-50 bg-primary-red text-white p-2 w-full rounded-none rounded-b-md">
-                    Make Payment
-                  </Button>
-                </Card>
-              </SwiperSlide>
-            );
-          })}
-          <PaymentSwiperButton
-            currentIndex={currentIndex}
-            maxIndex={payments.length - 1}
-            containerStyles="z-10 w-full gap-2 absolute bottom-[calc(50%_-_22px)]  flex justify-between"
-            btnStyles={cn(
-              "flex items-center justify-center text-white bg-primary-gray p-2 w-[32px] h-[32px] rounded-full"
-            )}
-          />
-        </Swiper>
-      </div>
-      {/* Pagination indicators */}
-      <div className="flex justify-center gap-2 mt-4">
-        {payments.map((_, index) => (
-          <div
-            key={index}
-            className={`h-2 w-2 rounded-full ${
-              index === currentIndex ? "bg-primary-red" : "bg-gray-300"
-            }`}
-          />
-        ))}
-      </div>
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={slidesPerView}
+        onSlideChange={handleSlideChange}
+        className={cn(
+          "flex items-center justify-center",
+          swiperType === "sm" ? "" : "w-[500px]"
+        )}
+      >
+        {payments.map((payment, index) => {
+          return (
+            <SwiperSlide key={index} className="z-0">
+              <Card className="relative overflow-hidden w-[220px] min-h-[181px]  mx-auto">
+                <div className="p-4">
+                  <Badge variant="outline" className="mb-2 w-full rounded-full">
+                    {payment.id}
+                  </Badge>
+                  <div className="text-gray-700 font-thin text-sm mb-2">
+                    {payment.description}
+                  </div>
+                  <div className="text-2xl">
+                    CAD {payment.amount.toFixed(2)}
+                  </div>
+                </div>
+
+                <Button className="absolute bottom-0 right-0 z-50 bg-primary-red text-white p-2 w-full rounded-none rounded-b-md">
+                  Make Payment
+                </Button>
+              </Card>
+            </SwiperSlide>
+          );
+        })}
+        <PaymentSwiperButton
+          currentIndex={currentIndex}
+          maxIndex={
+            swiperType === "sm" ? payments.length - 1 : payments.length - 2
+          }
+          containerStyles="z-10 w-full gap-2 absolute bottom-[calc(50%_-_22px)] right-0 left-0 flex justify-between"
+          btnStyles={cn(
+            "flex items-center justify-center text-white bg-primary-gray p-2 w-[32px] h-[32px] rounded-full"
+          )}
+        />
+      </Swiper>
     </div>
   );
 }
