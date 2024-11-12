68510aaff369498ba680a5a34fb4938a34e9968a
Joao Sinhasique <sinhasique12@gmail.com>
2024-11-12 10:44:22 -0800

    team members section responsive

--- front-end/immigration-app/src/components/aboutUs/Team.tsx
+++ front-end/immigration-app/src/components/aboutUs/Team.tsx
@@ -3,16 +3,27 @@ import larissa from "@/assets/larissa_up_immigration.png";
 import Image from "next/image";
 import member1 from "@/assets/aboutUs/man.jpeg";
 import member2 from "@/assets/aboutUs/woman.jpeg";
+import Link from "next/link";
+
 export default function Team() {
   return (
-    <div>
-      {" "}
-      <div className="flex gap-4 mb-2">
-        <Image src={larissa} alt="Larissa Castelluber" className="w-2/4" />
-        <div>
-          <h3 className="font-semibold">Larissa Castelluber</h3>
+    <div className="p-4">
+      {/* higher section */}
+      <div className="flex flex-col md:flex gap-4 mb-4">
+        {/* Larissa's picture */}
+        <Image
+          src={larissa}
+          alt="Larissa Castelluber"
+          className="w-full md:w-96 md:h-96"
+        />
+
+        {/* text description of Larissa */}
+        <div className="w-full">
+          <h3 className="font-semibold text-lg md:text-xl">
+            Larissa Castelluber
+          </h3>
           <p className="text-xs text-gray-500 mb-3">RCIC</p>
-          <div className="">
+          <div className="text-sm md:text-base">
             <p className="mb-5">
               Meet Larissa, a highly qualified and dedicated Regulated Canadian
               Immigration Consultant (RCIC). Larissa is a member of the
@@ -30,18 +41,40 @@ export default function Team() {
           </div>
         </div>
       </div>
-      <div>
-        <div className="flex gap-5">
+
+      {/* lower section with the 2 other employees */}
+      <div className="flex flex-col md:flex-row gap-4 items-center">
+        <div className="relative w-48 h-48">
           <Image
             src={member1}
-            alt="Larissa Castelluber"
-            className="w-52 h-52 object-cover rounded-lg"
+            alt="Member 1"
+            className="w-full h-full object-cover rounded-2xl"
           />
+          <div className="absolute bottom-0 left-3 text-white">
+            <p className="font-semibold">Name</p>
+            <p>Position</p>
+          </div>
+        </div>
+
+        <div className="relative w-48 h-48">
           <Image
             src={member2}
-            alt="Larissa Castelluber"
-            className="w-52 h-52 object-cover rounded-lg"
+            alt="Member 2"
+            className="w-full h-full object-cover rounded-2xl"
           />
+          <div className="absolute bottom-0 left-3 text-white">
+            <p className="font-semibold">Name</p>
+            <p>Position</p>
+          </div>
+        </div>
+
+        {/* Consultation button */}
+        <div className="mt-4 md:mt-0">
+          <Link href="/consultation">
+            <button className="bg-primary-red text-white py-2 px-9 rounded-xl font-semibold">
+              Book a consultation
+            </button>
+          </Link>
         </div>
       </div>
     </div>
