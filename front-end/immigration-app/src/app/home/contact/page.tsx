89012031cd8992a04c9671c904de80b27129d1df
Joao Sinhasique <sinhasique12@gmail.com>
2024-11-13 11:46:08 -0800

    how can we help component

--- front-end/immigration-app/src/app/home/contact/page.tsx
+++ front-end/immigration-app/src/app/home/contact/page.tsx
@@ -1,5 +1,10 @@
+import HowCanWeHelp from "@/components/contactUs/HowCanWeHelp";
 import React from "react";
 
 export default function page() {
-  return <div>Contact page</div>;
+  return (
+    <div className="px-4 sm:px-8 lg:px-16 pb-10 mt-10">
+      <HowCanWeHelp />
+    </div>
+  );
 }
