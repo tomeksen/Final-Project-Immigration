81b05aff68138930650f409ffa3174729c8e46c8
Misa Takeda <misatakeda.2799@gmail.com>
2024-12-10 07:02:42 +0900

    add sidebar

--- front-end/immigration-app/src/app/playground/page.tsx
+++ front-end/immigration-app/src/app/playground/page.tsx
@@ -1,18 +1,19 @@
 import { AppSheet } from "@/components/AppSheet";
 import { ApplicationsTable } from "@/components/dashboard/applications/ApplicationsTable";
 import { TaskManager } from "@/components/dashboard/applications/TaskManager";
+import { AppSidebar } from "@/components/dashboard/sidebar/AppSideBar";
 import { TaskList } from "@/components/TaskList";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 const TestPage = () => {
   return (
     <>
-      <div className="p-6">
-        <ApplicationsTable />
-      </div>
-
-      <div className="p-6">
-        <TaskManager />
-      </div>
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="p-6">
+          <TaskManager />
+        </div>
+      </SidebarProvider>
     </>
   );
 };
