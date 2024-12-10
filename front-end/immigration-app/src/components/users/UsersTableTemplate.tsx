7ba517e286843d255caa37201e712d28947c0733
yusuken1121 <yusuken1121@gmail.com>
2024-12-09 17:19:58 -0800

    Implement table template component

---
+++ front-end/immigration-app/src/components/users/UsersTableTemplate.tsx
@@ -0,0 +1,91 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
+import { User } from "@/type/Users.type";
+import { cn } from "@/lib/utils";
+import { Invitation } from "@/type/Invitation.type";
+
+interface Column {
+  header: string;
+  accessorKey: string;
+}
+
+interface TableTemplateProps {
+  columns: Column[];
+  data: User[] | Invitation[] | any[];
+  onAdd?: () => void;
+  onDelete?: (id: string) => void;
+}
+
+/**
+ * @param columns
+ * @returns
+ */
+
+export function UserTableTemplate({
+  columns,
+  data,
+  onAdd,
+  onDelete,
+}: TableTemplateProps) {
+  return (
+    <div className="space-y-4">
+      <Table>
+        <TableHeader className="bg-[#5E5E5E] text-white">
+          <TableRow>
+            {columns.map((column, index) => (
+              <TableHead
+                key={column.accessorKey}
+                className={cn(
+                  "p-4",
+                  index === 0 && "rounded-tl-md",
+                  index === columns.length - 1 && "rounded-tr-md"
+                )}
+              >
+                {/* ヘッダーの内容 */}
+                {column.header}
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {data.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {columns.map((column, colIndex) => (
+                <TableCell
+                  key={column.accessorKey}
+                  className={cn(
+                    "bg-white p-4",
+                    colIndex === 0 && "rounded-bl-md", // 最初の列の左下を丸くする
+                    colIndex === columns.length - 1 && "rounded-br-md" // 最後の列の右下を丸くする
+                  )}
+                >
+                  {column.accessorKey === "actions" && onDelete ? (
+                    <Button
+                      onClick={() => onDelete(row.id)}
+                      size="default"
+                      className="bg-primary-red"
+                    >
+                      <X className="h-4 w-4" />
+                      <span>DELETE</span>
+                    </Button>
+                  ) : (
+                    row[column.accessorKey]
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
