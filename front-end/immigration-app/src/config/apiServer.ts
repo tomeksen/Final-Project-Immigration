731274da858a0a59a8116563cba7abaecc750ff5
yusuken1121 <yusuken1121@gmail.com>
2024-12-17 17:03:10 -0800

    Update the custom fetch functionality

---
+++ front-end/immigration-app/src/config/apiServer.ts
@@ -0,0 +1,30 @@
+import { BASEURL } from "./apiClient";
+
+/**
+ * Sends a fetch request to a specified API endpoint.
+ *
+ * @param {string} endpoint - The API endpoint to call (e.g., "users", "posts/1").
+ * @param {RequestInit} [options={}] - Additional options for the fetch request, such as method, headers, and body.
+ * @returns {Promise<any>} - A promise that resolves to the JSON response from the API.
+ * @throws {Error} - Throws an error if the response status is not ok.
+ */
+export const apiServerFetch = async (
+  endpoint: string,
+  options: RequestInit = {}
+) => {
+  const baseURL = BASEURL || "http://localhost:3000";
+
+  const { headers, ...restOptions } = options;
+  const response = await fetch(`${baseURL}/${endpoint}`, {
+    ...restOptions,
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+};
